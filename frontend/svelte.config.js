04a04b61787b2e39e856766a2b137b2543433bbc
Bipasha Kumar <bipashakumar02@gmail.com>
2025-12-25 17:40:03 +0530

    Configure SvelteKit for static Vercel deploy

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess()],
+  preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    ssr: false, // 👈 disables server-side rendering
+    prerender: {
+      handleHttpError: 'warn'
+    }
   }
 };
 
